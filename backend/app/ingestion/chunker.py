d27f12e38e3b1ae97959db2aeb21eae074d3408b
unknown <shuhambisht758@gmail.com>
2026-07-02 11:57:40 +0530

    add frontend

--- backend/app/ingestion/chunker.py
+++ backend/app/ingestion/chunker.py
@@ -4,22 +4,17 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 pdf_path = BASE_DIR / "data" / "hrPolicy.pdf"
 
 loader = PyPDFLoader(str(pdf_path))
-
 documents = loader.load()
 
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size=800,
-    chunk_overlap=150
+    chunk_size=500,
+    chunk_overlap=100
 )
-
 chunks = splitter.split_documents(documents)
 
 print(f"Total Chunks: {len(chunks)}")
-
 print("\nFirst Chunk:\n")
-
 print(chunks[0].page_content)
